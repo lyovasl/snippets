c577892d16ad284ccb5cb30240f2bebe266ef099
Levon Aslanyan <aslanyan2012@mail.ru>
2024-03-03 23:55:42 +0400

    page id uniq params and search params

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,6 +1,33 @@
+import { redirect } from "next/navigation";
+import { db } from "@/db";
+
 export default function SnippetCreatePage() {
+  const createSnippets = async (formData: FormData) => {
+    //* this needs to be server action
+    "use server";
+
+    //* check the users inputs and make sure they re valid
+
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+
+    //* create a new record in the database
+
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+    console.log(snippet, "====");
+
+    //* Redirect the user back to the root route
+
+    redirect("/");
+  };
+
   return (
-    <form>
+    <form action={createSnippets}>
       <h3 className="font-bold m-3">Create a Sinppet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
