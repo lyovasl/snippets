dea5ba12925be870c2fe3adda45a1f050dd726c3
Levon Aslanyan <aslanyan2012@mail.ru>
2024-03-05 00:23:18 +0400

    finish search params

---
+++ src/app/snippets/[id]/loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const SnippetShowLoadingPage = (props: Props) => {
+  return <div>Loading...</div>;
+};
+
+export default SnippetShowLoadingPage;
---
+++ src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,13 @@
+type Props = {};
+
+const SnippetsNotFound = (props: Props) => {
+  return (
+    <div>
+      <h1 className="text-xl bold">
+        Soory, but we couldnt find that particular snippet
+      </h1>
+    </div>
+  );
+};
+
+export default SnippetsNotFound;
